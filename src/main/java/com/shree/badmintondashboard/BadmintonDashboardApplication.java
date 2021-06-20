ff8b145f41cc7f253226721f1898d139233045f1
Shree36123 <reachoutshree@gmail.com>
2021-06-20 18:40:40 +0530

    intial commit

---
+++ src/main/java/com/shree/badmintondashboard/BadmintonDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.shree.badmintondashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadmintonDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadmintonDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shree/badmintondashboard/BadmintonDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shree.badmintondashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadmintonDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
